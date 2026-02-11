e36e2220bd5a023b3ee333de2a11a23d6aaa2766
Raghavendra-Prisio <vk.raghavendra@prisiotechnologies.com>
2026-02-11 16:57:14 +0530

    changed url to match amplify in msalconfig

--- src/msalConfig.js
+++ src/msalConfig.js
@@ -3,8 +3,7 @@ export const msalConfig = {
     clientId: "8f3ff581-7d83-4030-bb36-80dfe9ff3bb4",
     authority:
       "https://login.microsoftonline.com/2ced8eca-fb8b-4dd6-a8af-ee0467e04fbc",
-    redirectUri:
-      "https://obe-static-website.s3.us-east-1.amazonaws.com/index.html",
+    redirectUri: "https://testing.d35rxtlumzro3.amplifyapp.com/",
   },
   cache: {
     cacheLocation: "sessionStorage", // safer
